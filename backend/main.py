6c7ff9aa26d72b15addfc70756ae6e8428350b45
BakedSoups <overbakedrice@gmail.com>
2026-05-27 11:40:59 -0700

    feat: containerized in docker + fixed fast api port

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,9 @@ app = FastAPI(title="Data Bucket API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[
+    "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
